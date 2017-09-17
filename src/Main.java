7b904fd28ee9b1598d346481f41de3318ca25b9e
Vlad Maznyi 550503 <vladmaznyi@gmail.com>
2017-09-17 15:03:07 +0300

    first step

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("PRIVET");
+        int a = 3;
+        int b = a * 8;
+        System.out.println("LOL " + b);
+    }
+
+}
